b48a3b897e927cc2bad41dea41f0e380ab146433
Sara <sara@Saras-MacBook-Pro.local>
2025-08-22 17:02:33 -0700

    remove firebase login for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,7 @@ export default function RootLayout({
     <ClerkProvider
       signInUrl="/sign-in"
       signUpUrl="/sign-up"
-      afterSignInUrl="/dashboard"
-      afterSignUpUrl="/dashboard"
+      
 
     >
       <html lang="en">
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,45 +1,24 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { SignIn, useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
-import { auth as firebaseAuth } from '../../../lib/firebase';
-import { signInWithCustomToken } from 'firebase/auth';
+
 
 function SignInPage() {
-  const { user, isLoaded, isSignedIn } = useUser();
+  const {  isLoaded, isSignedIn } = useUser();
   const router = useRouter();
-  const [firebaseReady, setFirebaseReady] = useState(false);
 
-  useEffect(() => {
-    async function signInToFirebase() {
-      try {
-        const res = await fetch('/api/firebase');
-        const { token } = await res.json();
 
-        if (token) {
-          await signInWithCustomToken(firebaseAuth, token);
-          console.log('✅ Signed into Firebase');
-          setFirebaseReady(true);
-        } else {
-          console.error('❌ No Firebase token received');
-        }
-      } catch (err) {
-        console.error('🔥 Firebase sign-in failed:', err);
-      }
-    }
+    
 
-    if (isSignedIn && isLoaded && !firebaseAuth.currentUser) {
-      signInToFirebase();
-    }
-  }, [isSignedIn, isLoaded]);
 
   // Redirect after Firebase is signed in
   useEffect(() => {
-    if (firebaseReady) {
+    if (isSignedIn) {
       router.replace('/dashboard');
     }
-  }, [firebaseReady]);
+  }, [isLoaded]);
 
   return (
     <div className="min-h-screen flex items-center bg-white justify-center">
